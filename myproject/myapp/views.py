648fcf082327790a798dd9d120aa22294b4406d1
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-09-09 18:31:02 -0700

    Adds more code from django course

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    context = {
+        'name': 'Todd',
+        'age': 33,
+        'nationality': 'American',
+    }
+    return render(request, 'index.html', context)
+
+def contact(request):
+    return HttpResponse('<h1>Contact Page</h1>')
