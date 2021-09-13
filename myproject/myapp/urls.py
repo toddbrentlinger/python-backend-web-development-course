6cae06e95738295f63a87af28cbef90d933f7bcc
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-09-12 20:20:52 -0700

    Adds simple word counter app

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,9 @@
+from os import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('contact/', views.contact, name = 'contact')
+    path('contact/', views.contact, name = 'contact'),
+    path('counter', views.counter, name = 'counter')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context = {
-        'name': 'Todd',
-        'age': 33,
-        'nationality': 'American',
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
 def contact(request):
     return HttpResponse('<h1>Contact Page</h1>')
+
+def counter(request):
+    context = {
+        'amount': len(request.GET['text'].split())
+    }
+    return render(request, 'counter.html', context)
