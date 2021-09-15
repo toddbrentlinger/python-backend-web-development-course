b30f667db8eacda1aeeff3ad574659f1915f46d6
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-09-14 18:23:19 -0700

    Adds functionality that uses Django model

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Feature:
+    name: str
+    details: str
+    id: int
+    is_true: bool
+
+    def __init__(self, name, details, id):
+        self.name = name
+        self.details = details
+        self.id = id
+        self.is_true = bool(id%2)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature('Fast', 'Our service is very quick', 0)
+    feature2 = Feature('Reliable', 'Our service is very reliable', 1)
+    feature3 = Feature('Easy to Use', 'Our service is easy to use', 2)
+    feature4 = Feature('Affordable', 'Our service is affordable', 3)
+    return render(request, 'index.html', {'features': [feature1, feature2, feature3, feature4]})
 
 def contact(request):
     return HttpResponse('<h1>Contact Page</h1>')
