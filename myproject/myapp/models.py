49bf6b8644776956fd3df9f59519937fe5a15958
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-09-17 18:57:32 -0700

    Adds Feature model to database and adds features to the database using the Django admin panel

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    name: str
-    details: str
-    id: int
-    is_true: bool
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
 
-    def __init__(self, name, details, id):
-        self.name = name
-        self.details = details
-        self.id = id
-        self.is_true = bool(id%2)
+    # def __init__(self, name, details, id):
+    #     self.name = name
+    #     self.details = details
+    #     self.id = id
+    #     self.is_true = bool(id%2)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,11 +4,12 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature('Fast', 'Our service is very quick', 0)
-    feature2 = Feature('Reliable', 'Our service is very reliable', 1)
-    feature3 = Feature('Easy to Use', 'Our service is easy to use', 2)
-    feature4 = Feature('Affordable', 'Our service is affordable', 3)
-    return render(request, 'index.html', {'features': [feature1, feature2, feature3, feature4]})
+    # feature1 = Feature('Fast', 'Our service is very quick', 0)
+    # feature2 = Feature('Reliable', 'Our service is very reliable', 1)
+    # feature3 = Feature('Easy to Use', 'Our service is easy to use', 2)
+    # feature4 = Feature('Affordable', 'Our service is affordable', 3)
+    features = Feature.objects.all()
+    return render(request, 'index.html', {'features': features})
 
 def contact(request):
     return HttpResponse('<h1>Contact Page</h1>')
