292e872226d809db952261a4cbf6839b287cddaf
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-10-05 17:59:51 -0700

    Finishes blog tutorial

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'post.html', {'post': post})
