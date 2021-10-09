16b45493084cc8b2c1c8b717fc06e4303841f57f
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-10-08 17:45:11 -0700

    Continues weather app with url routing.

--- weatherdetector/weatherdetector/settings.py
+++ weatherdetector/weatherdetector/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'weatherdetector.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
