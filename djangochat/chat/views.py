08cc5e1a98d551d73af535630476adfe6f566acc
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-10-13 17:51:48 -0700

    Continues realtime chat application tutorial

--- djangochat/chat/views.py
+++ djangochat/chat/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def room(request, room):
+    return render(request, 'room.html')
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if not Room.objects.filter(name=room).exists():
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+
+    return redirect('/'+room+'/?username='+username)
