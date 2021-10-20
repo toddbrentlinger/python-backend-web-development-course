6e84f7a113628f47e2ff4b0a9f33098e176a8d48
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-10-20 14:07:41 -0700

    Finishes django rest framework tutorial project

--- restframeworkproject/restframeworkproject/settings.py
+++ restframeworkproject/restframeworkproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drfapp',
 ]
 
@@ -126,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
--- restframeworkproject/restframeworkproject/views.py
+++ restframeworkproject/restframeworkproject/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from drfapp.serializers import StudentSerializer
 from drfapp.models import Student
 
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
-        data = {
-            'username': 'admin',
-            'years_active': 10,
-        }
-        return Response(data)
+        querySet = Student.objects.all()
+
+        # Returns all Student objects
+        # serializer = StudentSerializer(querySet, many=True)
+        
+        # Returns single Student object
+        studentFirst = querySet.first()
+        serializer = StudentSerializer(studentFirst)
+        
+        return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
         serializer = StudentSerializer(data=request.data)
